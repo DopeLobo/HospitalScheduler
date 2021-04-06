99da4132b3ed7ab86ad2924fe2e0ec885e6d473e
Isaac Carrasco <carrasci@unm.edu>
2021-04-06 16:43:22 -0600

    Adding Title/BackgroundColor/Clinicbutton

--- HospitalScheduler/Form1.Designer.cs
+++ HospitalScheduler/Form1.Designer.cs
@@ -31,13 +31,15 @@ namespace HospitalScheduler
         {
             this.btnEditProviders = new System.Windows.Forms.Button();
             this.btnMakeSchedule = new System.Windows.Forms.Button();
+            this.btnEditClinics = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnEditProviders
             // 
             this.btnEditProviders.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnEditProviders.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditProviders.Location = new System.Drawing.Point(283, 138);
+            this.btnEditProviders.Location = new System.Drawing.Point(283, 130);
             this.btnEditProviders.Name = "btnEditProviders";
             this.btnEditProviders.Size = new System.Drawing.Size(245, 86);
             this.btnEditProviders.TabIndex = 0;
@@ -49,7 +51,7 @@ namespace HospitalScheduler
             // 
             this.btnMakeSchedule.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.btnMakeSchedule.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMakeSchedule.Location = new System.Drawing.Point(283, 244);
+            this.btnMakeSchedule.Location = new System.Drawing.Point(283, 341);
             this.btnMakeSchedule.Name = "btnMakeSchedule";
             this.btnMakeSchedule.Size = new System.Drawing.Size(245, 86);
             this.btnMakeSchedule.TabIndex = 0;
@@ -57,16 +59,42 @@ namespace HospitalScheduler
             this.btnMakeSchedule.UseVisualStyleBackColor = false;
             this.btnMakeSchedule.Click += new System.EventHandler(this.btnMakeSchedule_Click);
             // 
+            // btnEditClinics
+            // 
+            this.btnEditClinics.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnEditClinics.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditClinics.Location = new System.Drawing.Point(283, 236);
+            this.btnEditClinics.Name = "btnEditClinics";
+            this.btnEditClinics.Size = new System.Drawing.Size(245, 86);
+            this.btnEditClinics.TabIndex = 1;
+            this.btnEditClinics.Text = "Edit Clinics";
+            this.btnEditClinics.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harrington", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 76);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quick Scheduler";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEditClinics);
             this.Controls.Add(this.btnMakeSchedule);
             this.Controls.Add(this.btnEditProviders);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Quick Scheduler";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +102,8 @@ namespace HospitalScheduler
 
         private System.Windows.Forms.Button btnEditProviders;
         private System.Windows.Forms.Button btnMakeSchedule;
+        private System.Windows.Forms.Button btnEditClinics;
+        private System.Windows.Forms.Label label1;
     }
 }
 
